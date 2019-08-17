14431a20d3e3f68d6d7b756ee855f517aa278aee
Aloysio <aloysiogl@gmail.com>
2019-08-17 13:21:02 -0300

    Added tests file

---
+++ Printer_Software_Java/Test_Serial_Comm/src/main/tests/MyTests.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+
+public class MyTests {
+
+    @Test
+    public void speedConversionTest(){
+
+    }
+}
